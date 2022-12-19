9b4ba0004f4e50ab306ff9aaa24f596e0865b62a
HaroldCC <haroldcruzcorrea.com>
2022-12-19 09:16:55 -0600

    dia7

---
+++ actividad_poo.py
@@ -0,0 +1,33 @@
+class Persona:
+    
+    def __init__(self,):
+        self.nombre=input("Ingrese el nombre: ")
+        self.edad =int(input("Ingrese la edad: "))
+        
+    def imprimir(self):
+        print("Nombre: " ,self.nombre)
+        print("Edad: " ,self.edad)
+        
+class Ciudadano(Persona):
+    
+    
+    def __init__(self):
+        super().__init__()
+        self.deposito= float(input("Ingrese el dinero a depositar: "))
+        
+    def imprimir(self):
+        super().imprimir()
+        print("Depósito: ",self.deposito)
+        
+    def impuestos(self):
+        if self.deposito > 4000:
+            print(f'El ciudadano {self.nombre} debe pagar impuestos')
+        else:
+            print(f'El ciudadano {self.nombre} no debe pagar impuestos')
+            
+    # Instancias
+    
+    ciudadano1= Persona()
+    ciudadano1.imprimir()
+    ciudadano1.impuestos()
+    
